5703fec5c8b8dab46e7bf93ee1b606fc812fc60b
Cosney <mateuszolsz29@gmail.com>
2025-05-04 02:32:34 +0200

    Refactor EzBio class to accept proxies and improve user-agent handling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from curl_cffi import requests
 from loguru import logger
 
 class EzBio:
-    def __init__(self):
+    def __init__(self, proxies: list):
         self.session = requests.Session()
 
         self.session.headers = {
@@ -20,17 +20,26 @@ class EzBio:
             'sec-fetch-mode': 'cors',
             'sec-fetch-site': 'same-site',
             'sec-gpc': '1',
-            'user-agent': f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.{random.randint(1000, 4000)}.0 Safari/537.36'
         }
+
+    def get_random_proxy(self):
+        return random.choice(proxies)
+    
+    def get_random_useragent(self):
+        return f'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.{random.randint(1000, 4000)}.0 Safari/537.36'
 
     def send_views(self, username: str):
-        proxies = random.choice(open('proxy.txt').read().splitlines())
-        proxy = {   
-            'http': f'http://{proxies}',
-            'https': f'http://{proxies}',
+        useragent = self.get_random_useragent()
+
+        self.session.headers['user-agent'] = useragent
+
+        proxy = self.get_random_proxy()
+        proxies = {
+            'http': 'http://'+proxy,
+            'https': 'http://'+proxy
         }
 
-        response = self.session.put(f'https://api.e-z.bio/bio/view/{username}', impersonate="edge101", proxies=proxy)
+        response = self.session.put(f'https://api.e-z.bio/bio/view/{username}', impersonate="edge101", proxies=proxies)
         if not response.json()['success']:
             logger.error('Failed to send view')
             return
@@ -41,7 +50,8 @@ class EzBio:
         while True:
             self.send_views(username)
 
-ezbio = EzBio()
+proxies = open('proxy.txt', 'r').read().splitlines()
+ezbio = EzBio(proxies)
 username = input('Enter username: ')
 threads = int(input('Enter number of threads: '))
 
